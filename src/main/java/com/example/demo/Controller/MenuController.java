da1dd9bf84415b963f65bae050c592aaed0d1e42
Алакен Кубатбеков <kubatbekovalaken@gmail.com>
2022-03-05 23:38:40 +0600

    Department redact added

---
+++ src/main/java/com/example/demo/Controller/MenuController.java
@@ -0,0 +1,47 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController {
+    @FXML
+    public Button employeeBtn;
+
+    @FXML
+    public Button departmentBtn;
+
+    public void openEmployeePage(ActionEvent event) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("employeeTable.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 730, 450);
+            stage.setTitle("База сотрудников");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openDepartmentPage(ActionEvent event) {
+        try{
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("departmentTable.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 700, 600);
+            stage.setTitle("База отделов");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
